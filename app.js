7afa916bfa0c73f2fe198c832828469b218cc9a5
Mattias Gulec <Voltair88@gmail.com>
2022-04-29 14:34:58 +0200

    fixed api key

--- app.js
+++ app.js
@@ -1,74 +1,74 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const blogRoutes = require('./routes/blogRoutes');
-const authRoutes = require('./routes/authRoutes');
-const cookieParser = require('cookie-parser');
-const {
-  requireAuth
-} = require('./middleware/authMiddleware');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const { requireAuth } = require("./middleware/authMiddleware");
 // express app
 const app = express();
 app.use(express.json());
 
 // connect to mongodb & listen for requests
-const dbURI = 'mongodb+srv://mattias:DkWSQSEaTUpiF5L@node-test.okcq1.mongodb.net/node-test?retryWrites=true&w=majority'
-mongoose.connect(dbURI, {
+const dbURI =
+  "mongodb+srv://mattias:DkWSQSEaTUpiF5L@node-test.okcq1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
-  .then(result => app.listen(3000))
-  .catch(err => console.log(err));
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // middleware & static files
-app.use(express.static('public'));
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(express.static("public"));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(cookieParser());
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
-
-app.get('/', (req, res) => {
-  res.redirect('/blogs');
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
 });
 
-app.get('/blogs/create', requireAuth, (req, res) => {
-  res.render('create', {
-    title: 'New Blog Entry'
-  })
-});
-
-
-app.get('/about', (req, res) => {
-  res.render('about', {
-    title: 'About'
+app.get("/blogs/create", requireAuth, (req, res) => {
+  res.render("create", {
+    title: "New Blog Entry",
   });
 });
 
-app.get('/signup', (req, res) => {
-  res.render('signup', {
-    title: 'Signup'
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
   });
 });
 
-app.get('/login', (req, res) => {
-  res.render('login', {
-    title: 'Login'
+app.get("/signup", (req, res) => {
+  res.render("signup", {
+    title: "Signup",
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login", {
+    title: "Login",
   });
 });
 
 //blog routes
-app.use('/blogs', blogRoutes)
+app.use("/blogs", blogRoutes);
 app.use(authRoutes);
 
 // 404 page
